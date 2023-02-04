b8ffaae6f2345db127e34679eb57a8e37841acd3
matias1364 <matiasleoncontreras@gmail.com>
2023-02-03 21:47:43 -0300

    change db

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Layout from '@/components/Layout'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import LoginPage from './login'
 import { useEffect, useState } from 'react'
+import { UserContext } from '@/context/UserContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,11 +14,28 @@ export default function Home() {
 
   const session=useSession()
   const supabase=useSupabaseClient()
+
   const [posts,setPosts]=useState([])
+  const [profile,setProfile]=useState(null)
 
   useEffect(()=>{
     selectPost()
   },[])
+
+  useEffect(()=>{
+    
+    if(!session?.user?.id){
+      return
+    }
+    supabase.from('profiles')
+        .select()
+        .eq('id', session.user.id)
+        .then(result=>{
+            if(result.data.length){
+                setProfile(result.data[0])
+            }
+        })
+  },[!session?.user?.id])
   if(!session){
     return <LoginPage/>
   }
@@ -31,11 +49,12 @@ export default function Home() {
   }
   return (
     <Layout>
-      <PostFormCard onPost={selectPost}/>
-      {posts.map(post =>( 
-      <PostCard {...post} key={post.id}/>)
-      )}
-      
+      <UserContext.Provider value={{profile}}>
+        <PostFormCard onPost={selectPost}/>
+        {posts.map(post =>( 
+        <PostCard {...post} key={post.id}/>)
+        )} 
+      </UserContext.Provider>
     </Layout>
   )
 }
